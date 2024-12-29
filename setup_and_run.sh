2106e87943a6bfb58a7a386f8f0bdd16712d7abb
ARPIT PRAJAPATI <getunknownerror@gmail.com>
2024-12-30 00:00:33 +0530

    added file setup_and_run.sh

---
+++ setup_and_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/GET-UNKNOWN-ERR0R/pdf-encryptor.git
+cd pdf-encryptor
+
+# Install the required library
+pip3 install PyPDF2
+
+# Run the Python script
+python3 pdf-encryptor.py
